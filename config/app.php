4644a50443128d840260cf9a39148b73c5bf2c51
Ratheepan <ratheepan93@hotmail.com>
2023-06-18 17:36:49 +0530

    Init GraphQL

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        // GraphQl
+        Nuwave\Lighthouse\Defer\DeferServiceProvider::class,
     ])->toArray(),
 
     /*
